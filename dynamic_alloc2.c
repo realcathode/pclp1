d7d5b8793c8c193fd2863afc010a7ba407fbc738
realcathode <ion.miron@tutanota.com>
2024-11-25 13:02:18 +0200

    Add `dynamic_alloc2.c`

---
+++ dynamic_alloc2.c
@@ -0,0 +1,14 @@
+/*
+    [3p] Să se scrie un program care citeşte de la tastatură două matrice: una inferior triunghiulară
+    (toate elementele de deasupra diagonalei principale sunt nule), şi cealaltă superior triunghiulară.
+    Ele vor fi reprezentate în memorie cât mai compact cu putinţă (fară a stoca şi zerourile de
+    deasupra, respectiv dedesubtul diagonalei). Se va calcula apoi produsul celor matrice, şi se va
+    afişa.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+}
